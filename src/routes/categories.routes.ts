d82bf3d18995a2db9fc2ba1ebd731668ca66dfad
Ana Gabriela <anagabrieladev@gmail.com>
2022-06-24 13:16:31 -0300

    feat: add uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 categoriesRoutes.post("/categories", (req, res) => {
   const { name, description } = req.body;
-
-  categories.push({
+  const category = {
+    id: uuid(),
     name,
     description,
-  });
+  };
+  categories.push(category);
 
   return res.status(201).send();
 });
