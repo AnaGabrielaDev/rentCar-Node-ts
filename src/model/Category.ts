3d5ea6fec59731dc09ecf381cc62c603943f643f
Ana Gabriela <anagabrieladev@gmail.com>
2022-06-24 15:15:56 -0300

    refactor: add interface in model

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,5 +1,9 @@
 import { v4 as uuid } from "uuid";
 
+interface ICreateCategory {
+  name: string;
+  description: string;
+}
 class Category {
   id?: string;
 
@@ -9,8 +13,11 @@ class Category {
 
   created_at: Date;
 
-  constructor() {
+  constructor({ name, description }: ICreateCategory) {
     if (!this.id) this.id = uuid();
+    this.name = name;
+    this.description = description;
+    this.created_at = new Date();
   }
 }
 export { Category };
