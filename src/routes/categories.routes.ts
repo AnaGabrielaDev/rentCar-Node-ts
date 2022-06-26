9c5d60b0505e79c5e918d2535fa4a16512bfcbbf
Ana Gabriela <anagabrieladev@gmail.com>
2022-06-25 21:51:42 -0300

    feat: add list route and function and add function to verify if category already exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists)
+    return res.status(400).json({ message: "this category already exists" });
   categoriesRepository.create({
     name,
     description,
@@ -15,4 +19,9 @@ categoriesRoutes.post("/", (req, res) => {
   return res.status(201).send();
 });
 
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.status(200).json(all);
+});
 export { categoriesRoutes };
