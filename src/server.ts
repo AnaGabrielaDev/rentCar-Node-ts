d2b2970c61ec87b2f162409e2da7b749b021ae91
Ana Gabriela <anagabrieladev@gmail.com>
2022-09-29 00:54:35 -0300

    refactor: add console when server is running

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 import { router } from "./routes";
-import swagger from 'swagger-ui-express';
-import swaggerFile from './swagger.json';
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
@@ -10,4 +10,8 @@ app.use(express.json());
 app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile));
 app.use(router);
 
-app.listen(3333);
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
+});
