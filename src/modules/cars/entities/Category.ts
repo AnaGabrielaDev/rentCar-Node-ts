aa045bed86b42ef1382316e31a2658b659d39954
Ana Gabriela <anagabrieladev@gmail.com>
2022-09-29 01:25:23 -0300

    refactor: change model to entities

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,23 @@
+import { v4 as uuid } from 'uuid'
+
+interface ICreateCategory {
+  name: string
+  description: string
+}
+class Category {
+  id?: string
+
+  name: string
+
+  description: string
+
+  created_at: Date
+
+  constructor({ name, description }: ICreateCategory) {
+    if (!this.id) this.id = uuid()
+    this.name = name
+    this.description = description
+    this.created_at = new Date()
+  }
+}
+export { Category }
---
+++ src/modules/cars/entities/Specification.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidV4 } from 'uuid'
+
+interface ICreateSpecification {
+  name: string
+  description: string
+}
+
+class Specification {
+  id?: string
+
+  name: string
+
+  description: string
+
+  created_at: Date
+
+  constructor({ name, description }: ICreateSpecification) {
+    if (!this.id) this.id = uuidV4()
+    this.name = name
+    this.description = description
+    this.created_at = new Date()
+  }
+}
+
+export { Specification }
