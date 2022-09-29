474be5acf9f7353bcebb649ea7b115c046380894
Ana Gabriela <anagabrieladev@gmail.com>
2022-09-29 01:30:03 -0300

    refactor repository to async to implements interface

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,42 +1,49 @@
-import { Category } from "../../model/Category";
-import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
-class CategoriesRepository implements ICategoriesRepository {
-  private categories: Category[];
+/* eslint-disable no-use-before-define */
+import { Category } from '../../entities/Category'
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../contracts/ICategoriesRepository'
 
-  private static INSTANCE: CategoriesRepository;
+class CategoriesRepository implements ICategoriesRepository {
+  private readonly categories: Category[]
+
+  private static INSTANCE: CategoriesRepository
 
   private constructor() {
-    this.categories = [];
+    this.categories = []
   }
 
   public static getInstance(): CategoriesRepository {
-    if(!CategoriesRepository.INSTANCE) {
-      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository()
     }
 
-    return CategoriesRepository.INSTANCE;
-  };
+    return CategoriesRepository.INSTANCE
+  }
 
-  create({ name, description }: ICreateCategoryDTO): void {
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
     const category = new Category({
       name,
       description,
-    });
+    })
 
-    this.categories.push(category);
+    this.categories.push(category)
   }
 
-  list(): Category[] {
-    return this.categories;
+  async list(): Promise<Category[]> {
+    return this.categories
   }
 
-  findByName(name: string): Category {
+  async findByName(name: string): Promise<Category | null> {
     const findCategory = this.categories.find(
       (category) => category.name === name
-    );
+    )
 
-    return findCategory!;
+    if (!findCategory) return null
+
+    return findCategory
   }
 }
 
-export { CategoriesRepository };
+export { CategoriesRepository }
