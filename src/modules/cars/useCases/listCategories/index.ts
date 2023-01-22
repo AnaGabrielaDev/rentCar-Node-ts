55ebf0760bf31e63b4e8afec71679fc0ad67e510
AnaGabriela <anagabrieladev@gmail.com>
2023-01-22 19:16:07 -0300

    refactor: use prisma

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,8 +1,8 @@
-import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository'
+import { PrismaCategoriesRepository } from '../../repositories/implementations/prisma/PrismaCategoriesRepository'
 import { ListCategoriesController } from './ListCategoriesController'
 import { ListCategoriesUseCase } from './ListCategoriesUseCase'
 
-const categoriesRepository = CategoriesRepository.getInstance()
+const categoriesRepository = new PrismaCategoriesRepository()
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository)
 const listCategoriesController = new ListCategoriesController(
   listCategoriesUseCase
